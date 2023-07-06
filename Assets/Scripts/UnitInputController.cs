eba3086549f3dc7de422612581dbb83ec9473903
ProfA1602 <ProfA1602@lab.aca>
2023-07-05 19:07:49 -0500

    Seleccion multiple

--- Assets/Scripts/UnitInputController.cs
+++ Assets/Scripts/UnitInputController.cs
@@ -17,14 +17,20 @@ public class UnitInputController : MonoBehaviour
 
     public event UnityAction OnCancelSelection;
 
-    public static UnitInputController Instance {get; private set;}
+    public static UnitInputController Instance { get; private set; }
 
-    private void Awake() 
+    private bool mPressed = false;
+    private Vector3 mMultipleSelectionStartPosition;
+    private Vector3 mMultipleSelectionEndPosition;
+    private Vector3 center;
+    private Vector3 halfExtends;
+
+    private void Awake()
     {
         Instance = this;
     }
 
-    private void OnSelection(InputValue value )
+    private void OnSelection(InputValue value)
     {
         if (value.isPressed)
         {
@@ -46,16 +52,18 @@ public class UnitInputController : MonoBehaviour
                     {
                         UnitManager.Instance.SelectUnit(unit);
                     }
-                }else
+                }
+                else
                 {
                     if (!EventSystem.current.IsPointerOverGameObject())
                     {
                         UnitManager.Instance.SpawnUnit(hit.point);
                     }
-                    
+
                 }
             }
-        }else
+        }
+        else
         {
             Debug.Log("No esta presionado");
         }
@@ -90,6 +98,78 @@ public class UnitInputController : MonoBehaviour
                 // Mover las unidades
                 UnitManager.Instance.MoveUnits(hit.point);
             }
+        }
+    }
+
+    private void OnMultipleSelectionPress(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(new Vector2(
+                Mouse.current.position.x.value,
+                Mouse.current.position.y.value
+            ));
+
+
+            if (Physics.Raycast(
+                ray,
+                out RaycastHit hit,
+                float.MaxValue
+            ))
+            {
+                mMultipleSelectionStartPosition = hit.point;
+                mMultipleSelectionEndPosition = mMultipleSelectionStartPosition;
+                mPressed = true;
+            }
+            
+        }
+    }
+    private void OnMultipleSelectionRelease(InputValue value)
+    {
+        if (CurrentSpawningState == SpawningState.Selection &&  mPressed)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(new Vector2(
+               Mouse.current.position.x.value,
+               Mouse.current.position.y.value
+           ));
+
+
+            if (Physics.Raycast(
+                ray,
+                out RaycastHit hit,
+                float.MaxValue
+            ))
+            {
+                mMultipleSelectionEndPosition = hit.point;
+
+                center = new Vector3(
+                    (mMultipleSelectionStartPosition.x + mMultipleSelectionEndPosition.x) / 2f,
+                    0f,
+                    (mMultipleSelectionStartPosition.z + mMultipleSelectionEndPosition.z) / 2f
+                );
+
+                halfExtends = new Vector3(
+                    Mathf.Abs(mMultipleSelectionEndPosition.x - mMultipleSelectionStartPosition.x) / 2f,
+                    0.5f,
+                    Mathf.Abs(mMultipleSelectionEndPosition.z - mMultipleSelectionStartPosition.z) / 2f
+                );
+
+                var colliders = Physics.OverlapBox(
+                    center,
+                    halfExtends,
+                    Quaternion.identity,
+                    LayerMask.GetMask("Unit")
+                );
+                Debug.Log($"Colision:{colliders.Length}");
+                foreach (var collider in colliders)
+                {
+                    if (collider.TryGetComponent<Unit>(out Unit unit))
+                    {
+                        UnitManager.Instance.SelectUnit(unit);
+                    }
+                }
+            }
+            mPressed = false;
         }
     }
 }
